f2c52fca4583d651e0b65f030c16c38bf3b543cc
Tejas <tejas.patle@hbwsl.com>
2022-04-04 13:01:10 +0530

    Added new non heirarchical taxonomy in Book

--- WP_plugin.php
+++ WP_plugin.php
@@ -15,34 +15,63 @@ function register_custom_post_type_books(){
         'singular-name'=>'Book'
     );
     
+    $supports= array('title','editor','thumbnail','comments','excerpts');
+        
     $options = array(
         'labels' => $labels,
         'public'=> true,
         'rewrite'=>array('slug'=>'books'),
-        'taxonomies'=> array('book-catagory')
+        'supports'=>$supports,
+        'taxonomies'=> array('book-catagory','book-tag')
     );
-    
+
     register_post_type( "Book", $options);
 }
 
-add_action( 'init', 'register_custom_post_type_books');
+add_action('init', 'register_custom_post_type_books');
     
 
-function register_custom_custom_taxonomy_book_catagery(){
+function register_custom_heirarchical_taxonomy_book_catagery() {
 
     $labels=array(
-        'name'=>'Books Catagory',
+        'name'=>'Books Catagories',
         'singular-name'=>'Book Catagory'
     );
     
     $options = array(
         'labels' => $labels,
         'hierarchical'=> true,
-        'rewrite'=>array('slug'=>'book-catagory')
+        'rewrite'=> array('slug' => 'book-catagory'),
+        'show_admin_column'=>true
     );
     
-    register_taxonomy( 'book-catagory', array('books'), $options);
-
+    register_taxonomy('book-catagory', array('books'), $options);
 }
     
-add_action( 'init', 'register_custom_custom_taxonomy_book_catagery');
+add_action('init', 'register_custom_heirarchical_taxonomy_book_catagery', 0);
+
+
+ 
+function register_nonhierarchical_taxonomy_book_tag() {
+
+    $labels = array(
+        'name' => 'Book Tags',
+        'singular_name' =>  'Book Tag',
+        'parent_item' => null,
+        'parent_item_colon' => null,
+        'public'=> true
+    ); 
+
+    $options=array(
+        'hierarchical' => false,
+        'labels' => $labels,
+        'show_in_rest' => true,
+        'show_admin_column' => true,
+        'rewrite' => array( 'slug' => 'book-tag' ),
+    );
+
+    register_taxonomy('book-tag', 'books', $options);
+}   
+
+
+add_action('init', 'register_nonhierarchical_taxonomy_book_tag', 0 );
