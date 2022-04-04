cbe91bf15005e398180d11acf888b0bb334a05fe
Tejas <tejas.patle@hbwsl.com>
2022-04-04 12:24:20 +0530

    Initial Commit

---
+++ WP_plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP Book Plugin
+Plugin URI: http://localhost/WordPress/
+Description: This is my Book Plugin for WordPress
+Author: Tejas Patle
+Author URI: http://localhost/WordPress/
+Version: 1.0
+*/
+
+
+
