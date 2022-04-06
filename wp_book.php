355e5f44a77bba28d7d7853f3b7399b1070a2c4e
Tejas <tejas.patle@hbwsl.com>
2022-04-06 09:46:41 +0530

    This is msg

--- wp_book.php
+++ wp_book.php
@@ -1,14 +1,15 @@
 <?php
-/*
+/** 
 Plugin Name: WP Book Plugin
 Plugin URI: http://localhost/WordPress/
 Description: This is my Book Plugin for WordPress
 Author: Tejas Patle
 Author URI: http://localhost/WordPress/
-Version: 1.0
-*/
+Version: 1.0 
+ *******************/
 
-function register_custom_post_type_books(){
+function Register_Custom_Post_Type_books()
+{
 
     $labels= array(
         'name'=>'Books',
@@ -28,10 +29,13 @@ function register_custom_post_type_books(){
     register_post_type( "Book", $options);
 }
 
-add_action('init', 'register_custom_post_type_books');
+add_action('init', 'Register_Custom_Post_Type_books');
     
 
-function register_custom_heirarchical_taxonomy_book_catagery() {
+/**
+ *  Created custom hierarchical taxonomy 
+ * **/
+function Register_Custom_Hierarchical_Taxonomy_Book_catagery() {
 
     $labels=array(
         'name'=>'Books Catagories',
@@ -48,11 +52,15 @@ function register_custom_heirarchical_taxonomy_book_catagery() {
     register_taxonomy('book-catagory', array('books'), $options);
 }
     
-add_action('init', 'register_custom_heirarchical_taxonomy_book_catagery', 0);
+add_action('init', 'Register_Custom_Hierarchical_Taxonomy_Book_catagery', 0);
 
 
- 
-function register_nonhierarchical_taxonomy_book_tag() {
+/**
+ * Cretaing no hierarchical taxonomy 
+ *
+ * @return void
+ */
+function Register_Non_Hierarchical_Taxonomy_Book_tag() {
 
     $labels = array(
         'name' => 'Book Tags',
@@ -73,13 +81,14 @@ function register_nonhierarchical_taxonomy_book_tag() {
     register_taxonomy('book-tag', 'books', $options);
 }   
 
-add_action('init', 'register_nonhierarchical_taxonomy_book_tag', 0 );
+add_action('init', 'Register_Non_Hierarchical_Taxonomy_Book_tag', 0);
 
 
-
-//adding meta box for books 
-
-function book_meta_fields() {
+/**
+ * Creating fields for Book 
+ */
+function Book_Meta_fields() 
+{
     ?>
     <div>  
         
@@ -102,14 +111,26 @@ function book_meta_fields() {
     <?php
 }
 
-function add_book_meta_box() {
-    add_meta_box("book-meta-box", 'Book Meta Box', 'book_meta_fields', 'book');
+
+/**
+ * Creating  Meta Box For Books
+ *
+ * @return void
+ */
+function Add_Book_Meta_box() 
+{
+    add_meta_box("book-meta-box", 'Book Meta Box', 'Book_Meta_fields', 'book');
 }
 
-add_action('add_meta_boxes', 'add_book_meta_box');
+add_action('add_meta_boxes', 'Add_Book_Meta_box');
 
 
-function save_meta_data_book($post_id) {
+/**
+ * Saving the data in post meta table
+ * 
+ * @return void
+ */
+function Save_Meta_Data_book($post_id) {
 
     //Save data for author first name
     $field_data = $_POST['author-first-name'];
@@ -189,4 +210,136 @@ function save_meta_data_book($post_id) {
     }
 }
 
-add_action('save_post', 'save_meta_data_book');
+add_action('save_post', 'Save_Meta_Data_book');
+
+/**
+ * Creating settings page for book
+ *
+ * @return void
+ */
+function Book_Setting_Page_book()
+{
+    echo '<input type="submit" id="btn" name=Submit>';
+    
+}
+
+/**
+ * Adding a MEnu Page for Book
+ *
+ * @return void
+ */
+function Add_Menu_Page_book()
+{
+    add_menu_page('Books Setting', 'Books Setting', 'manage_options', 'book-setting-page', 'Book_Setting_Page_book');
+
+    add_submenu_page('book', 'Books Setting', 'Books Setting', 'manage_options', 'book-setting-page', 'Book_Setting_Page_book');
+}
+
+add_action('admin_menu', 'Add_Menu_Page_book');
+
+
+/**
+ * Function to diaplaying bbok details
+ */
+function Display_Book_deatils($atts, $content)
+{
+    $atts = shortcode_atts( array(
+        'id'         => 'Unknown',
+        'author_name'=> 'Unknown',
+        'year'       => 'Unknown',
+        'catagory'   => 'Unknown',
+        'tag'        => 'Unknown',
+        'publisher'  => 'Unknown'
+    ), 
+    $atts );
+
+    $content= add_filter('the_content', $content);
+
+    $content.='<div>';
+
+    $content.='ID of Book is '.$atts['id'] .'<br>';
+    $content.='Author of Book is '.$atts['author_name'] .'<br>';
+    $content.='Published Year of Book is '.$atts['year'] .'<br>';
+    $content.='Catagory of Book is '.$atts['catagory'] .'<br>';
+    $content.='Tags of Book is '.$atts['tag'] .'<br>';
+    $content.='Publisher of Book is '.$atts['publisher'] .'<br>';
+
+    $content.='</div>';
+
+    return $content;
+}
+
+/**
+ * Creating a shortcode
+ */
+function Shortcode_book($atts ,$content)
+{
+    return Display_Book_deatils($atts, $content);
+}
+
+add_shortcode('Book', 'Shortcode_book');    
+
+/**
+ * register widget
+ */
+
+function Create_Book_Catagory_widget() 
+{
+    register_widget('Book_Catagory_Widget');
+}
+
+add_action('widgets_init', 'Create_Book_Catagory_widget');
+
+
+/**class for Widget */
+class Book_Catagory_Widget extends WP_Widget 
+{
+    /**constructor for widget class
+
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            'book_catagory_widget', __("Book Catagory Widget"), array('description'=> __("WIdget For displaying book of same catagory"))
+        );
+    }
+
+    function widget($args, $instance)
+    {
+        $catagory = apply_filters('widget_title', $instance['catagory']);
+
+        echo $args['before_widget'];
+
+        if(!empty($instance['catagory']))
+        {
+            echo $args['before_title']. $catagory .$args['after_title'];
+        }
+
+        echo $args['after_widget'];
+    }
+    //widget setting
+    function form($instance)
+    {
+        if(isset($instance['catagory']))
+        {
+            $catagory= $instance['catagory'];  
+        }
+        else{
+            $catagory = __('New Catagory');
+        }
+
+        ?>
+            <div>
+                <label for='<?php echo $this->get_field_id('catagory') ?>'><?php _e("Catagory") ?></label>
+                <input class='widefat' type='text' id='<?php echo $this->get_field_id('catagory') ?>' name='<?php echo $this->get_field_name('catagory') ?>' value="<?php echo esc_attr($catagory); ?>" >
+            </div>
+        <?php
+    }
+
+    function update($new_instance, $old_instance)
+    {
+        $instance=array();
+        $instance['catagory'] =(!empty($new_instance['catagory'])) ? strip_tags($new_instance['catagory']) : "" ; 
+        return $instance; 
+    }
+}
